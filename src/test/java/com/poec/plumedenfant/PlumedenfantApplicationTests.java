0a0d6b1ffca201710b2d3f287c60a8c33d610017
Salima-POEC-JAVA <salimamabrouk7@gmail.com>
2024-11-26 10:51:51 +0100

    initial_commit

---
+++ src/test/java/com/poec/plumedenfant/PlumedenfantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poec.plumedenfant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumedenfantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
