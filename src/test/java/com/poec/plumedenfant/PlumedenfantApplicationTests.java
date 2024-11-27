dd50792395ef0b11003bc337b70850bff9646f18
chrismag06 <christophe.maggiore@libertysurf.fr>
2024-11-27 10:59:49 +0100

    test Mail et Mot de passe

--- src/test/java/com/poec/plumedenfant/PlumedenfantApplicationTests.java
+++ src/test/java/com/poec/plumedenfant/PlumedenfantApplicationTests.java
@@ -1,34 +1,37 @@
 package com.poec.plumedenfant;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}) // Exclure la configuration de la source de données
 class PlumedenfantApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-//	
-//	private void checkRegEx(String str, String RegEx) {
-//		assertTrue()
-//	}
-//	
+	
+	private void checkRegEx(String str, String RegEx) {
+		System.out.println(("checkRegEx"));
+	    assertTrue(str.matches(RegEx));
+	}
+	
 	@Test
 	void testMailValidity() {
-		System.out.println("Test 1");
-//		String email = "absc356@provider.com";
-//	    assertTrue(email.matches(".+@.+\\.[a-z]+"));
+		System.out.println("Test Mail Validity");
+		checkRegEx("absc356@provider.com",".+@.+\\.[a-z]+");
 	}
 
 	@Test
 	void testMotDePasse() {
-		System.out.println("Test 2");
-//		String motDePasse = "absc35#r.com";
-//	    assertTrue(motDePasse.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$"));
+		System.out.println("Test Mot De Passe");
+		checkRegEx("absc35#Acom","^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$");
 	}
 }
